813a756a45fa71b480a76484a45bc2f886f913cd
HP <umerfarooq@hotmail.com>
2021-04-26 01:48:08 +0500

    first commit

---
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+// import useGetPriceData from './useGetPriceData'
+// import { CAKE } from '../constants'
+
+const useGetDocumentTitlePrice = () => {
+  // const priceData = useGetPriceData()
+
+  const cakePriceUsd = 0.00
+  
+  // priceData ? parseFloat(priceData.data[CAKE.address].price) : 0
+
+  const cakePriceUsdString = 0.0
+    // Number.isNaN(cakePriceUsd) || cakePriceUsd === 0
+    //   ? ''
+    //   : ` - $${cakePriceUsd.toLocaleString(undefined, {
+    //       minimumFractionDigits: 3,
+    //       maximumFractionDigits: 3,
+    //     })}`
+
+  useEffect(() => {
+    document.title = `PancakeSwap${cakePriceUsdString}`
+  }, [cakePriceUsdString])
+}
+export default useGetDocumentTitlePrice
